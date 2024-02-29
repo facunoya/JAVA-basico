b73e77a73c626637f6a7ddcf41030168c16de899
Facundo Noya <facunoya@hotmail.com>
2024-02-29 20:21:53 -0300

    my first JAVA code

---
+++ JAVA/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main( String[] args){
+        
+        System.out.println("Hola, Java!");
+        //Cadena de caracteres/strings
+        String myString = "Esto es una cadena de texto";
+        System.out.println(myString);
+        myString = "Aca cambio el valor de la cadena de texto";
+        System.out.println(myString);
+        //Numeros enteros
+        Integer miEntero = 7;
+        System.out.println(miEntero);
+        miEntero = miEntero +4;
+        System.out.println(miEntero);
+        //Numeros con decimales
+        Double miDoble = 6.50;
+        System.out.println(miDoble); 
+    }
+}
