ee2f595955a8f18abf61123c2d3c4786ff5266e3
Facundo Noya <facunoya@hotmail.com>
2024-02-29 20:42:08 -0300

    more JAVA codes

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,19 +1,26 @@
-public class Main{
-    public static void main( String[] args){
-        
+public class Main {
+    public static void main(String[] args) {
+
         System.out.println("Hola, Java!");
-        //Cadena de caracteres/strings
+        // Cadena de caracteres/strings
         String myString = "Esto es una cadena de texto";
         System.out.println(myString);
         myString = "Aca cambio el valor de la cadena de texto";
         System.out.println(myString);
-        //Numeros enteros
+        // Numeros enteros
         Integer miEntero = 7;
         System.out.println(miEntero);
-        miEntero = miEntero +4;
+        miEntero = miEntero + 4;
         System.out.println(miEntero);
-        //Numeros con decimales
+        // Numeros con decimales
         Double miDoble = 6.50;
-        System.out.println(miDoble); 
+        System.out.println(miDoble);
+        System.out.println(miDoble + " " + myString + " " + miEntero);
+        // Booleanos
+        Boolean miBooleano = true;
+        System.out.println(miBooleano);
+        // null
+        miBooleano = null;
+        System.out.println(miBooleano);
     }
 }
